c306f0496de0a57377c743deed9e666d5ea34d05
github username <github email address>
2019-11-01 13:09:17 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,53 @@
 class Zoo
+	attr_accessor :name
+	attr_reader :location
+	@@all = []
 
+	def initialize(name, location)
+		@name = name
+		@location = location
+
+		@@all << self
+	end
+
+	def self.all 
+		@@all
+	end
+
+	def animals
+		Animal.all.select do |animal|
+			animal.zoo == self
+		end
+	end
+
+	def animal_species
+		#uniq_species = []
+		#animals.each do |animal|
+		#	uniq_species << animal.species
+		#end
+		#uniq_species.uniq
+
+		animals.map do |animal|
+			animal.species
+		end.uniq
+		#Animal.all.species.uniq
+	end
+
+	def find_by_species(species)
+		animals.select do |animal|
+			animal.species == species
+		end
+	end
+
+	def animal_nicknames
+		animals.map do |animal|
+			animal.nickname
+		end
+	end
+
+	def self.find_by_location(location)
+		@@all.select do |zoo|
+			zoo.location == location
+		end
+	end
 end
