4c092e022930c8d682bff3d507bfa3f8a2f05b64
jakarai <jakaraiforsythe@gmail.com>
2019-11-04 12:19:18 -0500

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,45 @@
+require 'pry'
+
 class Zoo
+    attr_reader :name, :location
+
+   @@all=[]
+
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animal_instance|
+           animal_instance.zoo == self
+        end
+    end
+
+    def animal_species
+        animals.map  {|animal_instance|animal_instance.species }.uniq 
+    end
+
+    def find_by_species(species)
+        animals.select do |animal_instance|
+            animal_instance.species == species
+        end
+    end
+
+    def animal_nicknames
+        animals.map {|animal_nicknames| animal_nicknames.nickname }
+    end
+
+    def self.find_by_location(location)
+        @@all.select {|found_location| found_location.location == location}
+
+    end
+
+    
 
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,22 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("bronx","africa")
+zoo2 = Zoo.new("brooklyn","america")
+zoo3 = Zoo.new("rose","america")
+zoo4 = Zoo.new("towy","africa")
+zoo5 = Zoo.new("mawy","africa")
+
+
+animal1 = Animal.new("tiger",90 , "slasher")
+animal2 = Animal.new("sloth",99 , "steady")
+animal3 = Animal.new("shark",120 , "biter")
+animal4 = Animal.new("whale",330 , "mike")
+
+animal1.zoo = zoo1
+animal2.zoo = zoo1
+animal3.zoo = zoo2
+animal4.zoo = zoo3
 
 
 binding.pry
