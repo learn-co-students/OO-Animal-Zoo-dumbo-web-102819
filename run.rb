07cc14d7942f26defc48b61881ddbaf2f4e58378
Imade Osifo <toxsik88@gmail.com>
2019-11-04 09:35:07 -0500

    finished all methods for OO Animal - Zoo

--- run.rb
+++ run.rb
@@ -1,10 +1,19 @@
+require "pry"
+
 require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
-require 'pry'
+# require 'pry'
 
 
-#Test your code here
+z1 = Zoo.new("zootopia", "new york")
+z2 = Zoo.new("zootopia2", "new new york")
+z3 = Zoo.new("zena", "new york")
+
+a1 = Animal.new("tiger", 100, "tiggy", z1)
+a2 = Animal.new("pig", 50, "piggy", z2)
 
 
-binding.pry
+# binding.pry
+
 puts "done"
+binding.pry
